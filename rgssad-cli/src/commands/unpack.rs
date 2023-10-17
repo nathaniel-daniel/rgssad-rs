4271f0d6bc576225cf0ccd2cf524fe046f705dfe
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2023-10-17 04:28:03 -0700

    Finish path sanitization

--- rgssad-cli/src/commands/unpack.rs
+++ rgssad-cli/src/commands/unpack.rs
@@ -1,4 +1,5 @@
-use anyhow::anyhow;
+use anyhow::bail;
+use anyhow::ensure;
 use anyhow::Context;
 use std::fs::File;
 use std::path::Component as PathComponent;
@@ -37,54 +38,57 @@ pub fn exec(options: Options) -> anyhow::Result<()> {
     let output = std::fs::canonicalize(&options.output)?;
 
     let mut last_error = Ok(());
-    'entry_loop: while let Some(entry) = reader.read_entry()? {
+    while let Some(entry) = reader.read_entry()? {
         println!("Extracting \"{}\"", entry.file_name());
 
         // Sanitize and build path
-        let mut out_path = construct_out_path(&output, entry.file_name().as_ref()).unwrap();
-
-        /*
-        let mut depth = 0_i32;
-        for component in Path::new(entry.file_name()).components() {
-            match component {
-                PathComponent::Prefix(_) => {
-                    println!("  encountered prefix in path, skipping");
-                    last_error = Err(anyhow!(
-                        "failed to extract \"{}\", encountered prefix in path",
-                        entry.file_name()
-                    ));
-
-                    break 'entry_loop;
-                }
-                PathComponent::RootDir => {
-                    println!("  encountered root dir in path, skipping");
-                    last_error = Err(anyhow!(
-                        "failed to extract \"{}\", encountered root dir in path",
-                        entry.file_name()
-                    ));
-
-                    break 'entry_loop;
-                }
-                PathComponent::CurDir => {}
-                PathComponent::ParentDir => {
-                    depth = match depth.checked_sub(1) {
-                        Some(depth) => depth,
-                        None => {
-                            println!("  encountered root dir in path, skipping");
-                        }
-                    }
-                }
-                _ => {
-                    dbg!(component);
-                }
+        let out_path = match construct_out_path(&output, entry.file_name().as_ref()) {
+            Ok(out_path) => out_path,
+            Err(error) => {
+                eprintln!("  {error}, skipping");
+                last_error =
+                    Err(error).with_context(|| format!("failed to extract {}", entry.file_name()));
+                continue;
             }
-        }
-        */
+        };
     }
 
     last_error
 }
 
 fn construct_out_path(out_dir: &Path, file_path: &Path) -> anyhow::Result<PathBuf> {
-    todo!()
+    let mut out_path = out_dir.to_path_buf();
+    let mut depth = 0_u32;
+
+    for component in file_path.components() {
+        match component {
+            PathComponent::Prefix(_) => {
+                bail!("encountered prefix in path");
+            }
+            PathComponent::RootDir => {
+                bail!("encountered root dir in path");
+            }
+            PathComponent::CurDir => {}
+            PathComponent::ParentDir => {
+                depth = depth.checked_sub(1).context("path goes above out path")?;
+            }
+            PathComponent::Normal(path) => {
+                depth = depth.checked_add(1).context("path depth overflow")?;
+
+                let path_has_prefix = Path::new(path)
+                    .components()
+                    .any(|path| matches!(path, PathComponent::Prefix(_)));
+                let path_has_root = Path::new(path)
+                    .components()
+                    .any(|path| matches!(path, PathComponent::RootDir));
+
+                ensure!(!path_has_prefix, "path component has prefix");
+                ensure!(!path_has_root, "path component has root");
+
+                out_path.push(path);
+            }
+        }
+    }
+
+    Ok(out_path)
 }
